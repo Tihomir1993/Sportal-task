612d6d863e9a26d35222c45f6620e68bde8923ad
Tihomir1993 <tihomir.todorov16@gmail.com>
2022-01-12 22:16:21 +0200

    add js code to generate from json object

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
         slider.classList.add('active');
         startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
         scrollLeft = slider.scrollLeft;
+        e.preventDefault();
     }
 
     const move = (e) => {
@@ -36,4 +37,63 @@ document.addEventListener("DOMContentLoaded", function() {
         slider.addEventListener('mouseup', end);
         slider.addEventListener('touchend', end);
     })();
+
+
+    const myJSON = [{
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари 2020 | 08: 01",
+        newsDescription: "Гешев с незабавна проверка за горене на отпадъци в ТЕЦ-овете на Ковачки",
+        status: "older"
+    }, {
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари 2020 | 08:01",
+        newsDescription: "Васил Божков помолил руснаци да му помогнат, за да излезе от ареста в Абу Даби",
+        status: "updated"
+    }, {
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари",
+        newsDescription: "Пореден въоръжен грабеж в София! Този път обраха магазин в кв. 'Изток'",
+        status: "older"
+    }, {
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари 2020 | 08: 01",
+        newsDescription: "Има задържан за банковия обир в София",
+        status: "updated"
+    }, {
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари 2020 | 08:01",
+        newsDescription: "Васил Божков помолил руснаци да му помогнат, за да излезе от ареста в Абу Даби",
+        status: "updated"
+    }, {
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари",
+        newsDescription: "Пореден въоръжен грабеж в София! Този път обраха магазин в кв. 'Изток'",
+        status: "older"
+    }, {
+        newsImage: "./assets/img/image-news.png",
+        newsDate: "13 февруари 2020 | 08: 01",
+        newsDescription: "Има задържан за банковия обир в София",
+        status: "updated"
+    }];
+    //const myObj = JSON.parse(myJSON);
+
+    const news = document.getElementById("js-news");
+
+    for (let index = 0; index < myJSON.length; index++) {
+        news.innerHTML += `
+        <a href='#' class='news-slide'> 
+            <div class='img-wrapper'>
+                <img src='${myJSON[index]["newsImage"]}' alt='' />
+             </div> 
+             <div class="date">${myJSON[index]["newsDate"]}</div>
+                <div class="description">
+                    <p>${myJSON[index]["newsDescription"]}</p>
+                </div>
+        </a>`;
+        if (myJSON[index]["status"] == 'updated') {
+            document.getElementsByClassName('news-slide')[index].classList.add('updated')
+            document.getElementsByClassName('img-wrapper')[index].innerHTML += '<div class="updated-img"><img src="./assets/img/image-update.png" alt="" /></div>'
+        }
+
+    }
 });
